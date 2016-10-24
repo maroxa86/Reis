6755168df46150ebbf84cbc9d2fe45e65ed8c5fe
Xavi <Xavi@192.168.1.38>
2016-10-24 20:54:31 +0200

    llistat paquets

--- reis/src/main/java/reis/bo/PaquetBO.java
+++ reis/src/main/java/reis/bo/PaquetBO.java
@@ -1,9 +1,13 @@
 package reis.bo;
 
+import java.util.List;
+
 import reis.beans.Paquet;
 
 public interface PaquetBO {
 
 	void insertar(Paquet paquet);
+
+	List<Paquet> carregarPaquets();
 
 }
---
+++ reis/src/main/java/reis/managedBeans/LlistatPaquetsManagedBeans.java
@@ -0,0 +1,48 @@
+package reis.managedBeans;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+import reis.beans.Paquet;
+import reis.bo.PaquetBO;
+
+@ManagedBean(name="llistatPaquetsManagedBeans")
+@RequestScoped
+public class LlistatPaquetsManagedBeans {
+
+	private List<Paquet> llistatPaquets;
+	
+	@ManagedProperty("#{paquetBO}")
+	private PaquetBO paquetBO;
+	
+	
+	public LlistatPaquetsManagedBeans(){
+		super();
+	}
+	
+	@PostConstruct
+	public void init(){
+		llistatPaquets = paquetBO.carregarPaquets();
+	}
+
+	public List<Paquet> getLlistatPaquets() {
+		return llistatPaquets;
+	}
+
+	public void setLlistatPaquets(List<Paquet> llistatPaquets) {
+		this.llistatPaquets = llistatPaquets;
+	}
+
+	public PaquetBO getPaquetBO() {
+		return paquetBO;
+	}
+
+	public void setPaquetBO(PaquetBO paquetBO) {
+		this.paquetBO = paquetBO;
+	}
+	
+}
